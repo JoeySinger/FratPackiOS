25d1dfdbbbe4d044a49ba660769e6a3797c7dcc3
Joey Singer <joeysinger7@gmail.com>
2018-09-09 18:20:51 -0400

    Initial Commit

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FratPack
+//
+//  Created by Joey Singer on 9/9/18.
+//  Copyright © 2018 FratPack. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
